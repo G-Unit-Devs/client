e1d216ae9e8c438464d0206940f05bd59785fde4
nakschadiii <105643348+nakschadiii@users.noreply.github.com>
2025-02-23 15:13:25 +0100

    push

--- components/ChatBox/Body.tsx
+++ components/ChatBox/Body.tsx
@@ -6,12 +6,15 @@ import { $ScrollView } from "@legendapp/state/react-native";
 import { useRef } from "react";
 import { ActivityIndicator, Pressable, ScrollView, Text, View } from "react-native";
 import tw from "twrnc";
+import MediaObject from "../MediaObject";
+import { FontAwesome } from '@expo/vector-icons';
 
 const loading$ = observable(false);
 socket.on('loading/chat/bot/box', loading$.set);
+socket.on('recommendation/pro', console.log);
 
 export default function ChatBoxBody() {
-    const { messages } = use$(chatBot$);
+    const chatBot = use$(chatBot$);
     const scrollView = useRef<ScrollView>(null);
     const loading = use$(loading$);
 
@@ -19,12 +22,15 @@ export default function ChatBoxBody() {
         setTimeout(() => { scrollView.current?.scrollToEnd(); }, 100);
     });
 
-    observe(chatBot$?.messages, () => {
+    if(chatBot$?.messages) observe(chatBot$?.messages, () => {
         setTimeout(() => { scrollView.current?.scrollToEnd(); }, 100);
     });
 
     return <$ScrollView style={tw`flex-1`} contentContainerStyle={tw`p-4 gap-1`} ref={scrollView}>
-        {loading ? <ActivityIndicator size="large" /> : messages.sort((a, b) => a.date - b.date).map((message, index) => <ChatBubble key={index} message={message} fromUser={message.sender === "user"} />)}
+        {loading ? <ActivityIndicator size="large" /> : chatBot?.messages.sort((a, b) => a.date - b.date).map((message, index) => {
+            if (message.type === "recommendation") return <Recommendation key={index} message={message} />
+            return <ChatBubble key={index} message={message} fromUser={message.sender === "user"} />
+        })}
     </$ScrollView>
 }
 
@@ -37,3 +43,27 @@ export function ChatBubble({ message, fromUser }: { message: { text: string }, f
         </Pressable>
     </View>
 }
+
+export function Recommendation({ message }) {
+    const { bg, muted, primary } = use$(theme$);
+
+    console.log(message);
+
+    return <View style={tw`flex items-start justify-start`}>
+        <Pressable style={tw`bg-${muted.bg} p-2 px-3 rounded-[20px] max-w-3/4`}>
+            <Text style={tw`text-${muted.fg}`}>{message.text}</Text>
+        </Pressable>
+        <View style={tw`flex gap-2 p-2`}>
+            {message.pros.map((user, index) => {
+                return <MediaObject
+                    key={index}
+                    text={user.username}
+                    title={user.firstname + " " + user.lastname}
+                    right={
+                        <FontAwesome name="send-o" size={24} color={tw`text-${primary.bg}`.color} />
+                    } />
+            })}
+        </View>
+    </View>
+}
+
